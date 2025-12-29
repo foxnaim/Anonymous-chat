226303b08c7c9b08e970d52ddc47bf124332a9af
Rick <118506963+foxnaim@users.noreply.github.com>
2025-12-29 17:53:36 +0500

    modified:   src/components/pages/admin/AdminSettings.tsx

--- src/components/pages/admin/AdminSettings.tsx
+++ src/components/pages/admin/AdminSettings.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
