f321ddaf8f79682c34539cc1c33cdcfa1cc39dcd
Rick <118506963+foxnaim@users.noreply.github.com>
2025-12-30 21:41:47 +0500

    modified:   src/components/pages/company/CompanySettings.tsx

--- src/components/pages/company/CompanySettings.tsx
+++ src/components/pages/company/CompanySettings.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { FiUpload, FiX, FiEdit2, FiEye, FiEyeOff } from "react-icons/fi";
+import { FiUpload, FiX, FiEdit2 } from "react-icons/fi";
 import { CompanyHeader } from "@/components/CompanyHeader";
 import { useAuth } from "@/lib/redux";
 import { useCompany, useUpdateCompany } from "@/lib/query";
